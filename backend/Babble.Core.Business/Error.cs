7afc8e8eedb8dbc380ae00b46b8947e27b41b231
ImoutoLily <shirley.fox_@hotmail.com>
2022-05-16 09:17:16 +0200

    Use predefined errors in the validation

--- backend/Babble.Core.Business/Error.cs
+++ backend/Babble.Core.Business/Error.cs
@@ -2,7 +2,7 @@ namespace Babble.Core.Business;
 
 public class Error
 {
-    public Error(string code, string severity, string message)
+    public Error(string code, string message, string severity = "Error")
     {
         Code = code;
         Severity = severity;
@@ -13,8 +13,18 @@ public class Error
     public string Severity { get; set; }
     public string Message { get; set; }
 
+    public static readonly Error InvalidEmail 
+        = new Error("1000", "Invalid email address");
+    
+    public static readonly Error InvalidUserName = 
+        new Error("1001", "Invalid username");
+
+    public static readonly Error InvalidPassword = 
+        new Error("1002", "Invalid password");
+    
     public static readonly Error EmailInUse 
-        = new Error("1000", "Error", "This email is already in use by someone else");
+        = new Error("1003", "This email is already in use by someone else");
+    
     public static readonly Error UserNameInUse 
-        = new Error("1001", "Error", "This username is already in use by someone else");
+        = new Error("1004", "This username is already in use by someone else");
 }
--- backend/Babble.Core.Business/Validators/UserAuthenticateValidator.cs
+++ backend/Babble.Core.Business/Validators/UserAuthenticateValidator.cs
@@ -10,17 +10,17 @@ public class UserAuthenticateValidator : AbstractValidator<UserAuthenticate>
         RuleFor(x => x.Email)
             .NotEmpty()
             .EmailAddress()
-            .WithErrorCode("4326")
-            .WithMessage("Invalid email adrress");
+            .WithErrorCode(Error.InvalidEmail.Code)
+            .WithMessage(Error.InvalidEmail.Message);
 
         RuleFor(x => x.UserName)
             .Matches("^(?=.*[a-zA-Z])[ a-zA-Z\\d\\-_]{4,32}$")
-            .WithErrorCode("4598")
-            .WithMessage("Invalid username");
+            .WithErrorCode(Error.InvalidUserName.Code)
+            .WithMessage(Error.InvalidUserName.Message);
 
         RuleFor(x => x.Password)
             .Matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,32}$")
-            .WithErrorCode("9345")
-            .WithMessage("Invalid password");
+            .WithErrorCode(Error.InvalidPassword.Code)
+            .WithMessage(Error.InvalidPassword.Message);
     }
 }
